ac6f74779ab3b4b9b9f319c843ed0784435a56bc
Chandler Watson <use.an.alias@gmail.com>
2017-05-21 15:13:30 -0700

    Add basic scaffolding

---
+++ setup.py
@@ -0,0 +1,20 @@
+# -*- coding: utf8 -*-
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(name='dream',
+      version='0.1.0',
+      description='An interactive art project using Google DeepDream for idea generation',
+      long_description=readme,
+      author='Chandler Watson',
+      author_email='watson@facni.com',
+      url='https://github.com/useanalias/dream',
+      license=license,
+      packages=find_packages(exclude=('tests', 'docs'))
+)
