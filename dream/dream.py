295b208e34b5dfe77298add25ece0fe426eb4f46
Chandler Watson <use.an.alias@gmail.com>
2017-05-21 17:03:09 -0700

    Fix merging of args and rc files

--- dream/dream.py
+++ dream/dream.py
@@ -1,5 +1,25 @@
-from docopt import docopt
-
-
 def dream(args):
-    print('To be implemented! Yay!')
+    if args['calibrate']:
+        calibrate(args)
+    elif args['push']:
+        push(args)
+    elif args['pull']:
+        pull(args)
+    elif args['status']:
+        status(args)
+
+
+def calibrate(args):
+    pass
+
+
+def push(args):
+    pass
+
+
+def pull(args):
+    pass
+
+
+def status(args):
+    instance_id = args['<id>']
